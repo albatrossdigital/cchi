de311dce6a938e44bf9437f47584cc692c3f9d79
Jennifer Mattson <jennifer@Jennifermattson.com>
2014-10-20 10:53:07 -0700

    more merge fixes

---
+++ sites/all/themes/custom/cchi_v2/templates/page.tpl.php
@@ -0,0 +1,265 @@
+<!--.page -->
+<div data-offcanvas role="document" class="page off-canvas-wrap"><div class="inner-wrap">
+
+  <!--.l-header region -->
+  <header role="banner" class="l-header">
+
+    <?php if ($top_bar): ?>
+      <!--.top-bar -->
+      <?php if ($top_bar_classes): ?>
+      <div class="<?php print $top_bar_classes; ?>">
+      <?php endif; ?>
+        <div class="left mobile-logo"><?php print $linked_logo; ?></div>
+        <nav class="top-bar" data-topbar<?php print $top_bar_options; ?>>
+          <ul class="title-area inner-wrap">
+            <li class="toggle-off-canvas menu-toggle right"><a class="left-off-canvas-toggle" href="#"><span><?php print $top_bar_menu_text; ?></span> <i class="fa-bars"></i></a></li>
+          </ul>
+          <section class="top-bar-section left-off-canvas-menu">
+            <a class="left-off-canvas-toggle" href="#"><span><?php print t('Close'); ?></span> <i class="fa-times"></i></a>
+            <?php if ($top_bar_main_menu) :?>
+              <?php print $top_bar_main_menu; ?>
+            <?php endif; ?>
+            <?php if ($top_bar_secondary_menu) :?>
+              <?php print $top_bar_secondary_menu; ?>
+            <?php endif; ?>
+            <?php print render($page['topbar']); ?>
+          </section>
+        </nav>
+      <?php if ($top_bar_classes): ?>
+      </div>
+      <?php endif; ?>
+      <!--/.top-bar -->
+    <?php endif; ?>
+    
+ 
+
+    <!-- Title, slogan and menu -->
+    <?php if ($alt_header): ?>
+    <section class="row <?php print $alt_header_classes; ?>">
+
+      <?php if ($linked_logo): print $linked_logo; endif; ?>
+
+      <?php if ($site_name): ?>
+        <?php if ($title): ?>
+          <div id="site-name" class="element-invisible">
+            <strong>
+              <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
+            </strong>
+          </div>
+        <?php else: /* Use h1 when the content title is empty */ ?>
+          <h1 id="site-name">
+            <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
+          </h1>
+        <?php endif; ?>
+      <?php endif; ?>
+
+      <?php if ($site_slogan): ?>
+        <h2 title="<?php print $site_slogan; ?>" class="site-slogan"><?php print $site_slogan; ?></h2>
+      <?php endif; ?>
+
+      <?php if ($alt_main_menu): ?>
+        <nav id="main-menu" class="navigation" role="navigation">
+          <?php print render($page['header']); ?>
+          <?php print ($alt_main_menu); ?>
+        </nav> <!-- /#main-menu -->
+      <?php endif; ?>
+
+      <?php if ($alt_secondary_menu): ?>
+        <nav id="secondary-menu" class="navigation" role="navigation">
+          <?php print $alt_secondary_menu; ?>
+        </nav> <!-- /#secondary-menu -->
+      <?php endif; ?>
+
+    </section>
+    <?php endif; ?>
+    <!-- End title, slogan and menu -->
+
+  </header>
+  <!--/.l-header -->
+  
+   <?php if (!empty($page['button_menu'])): ?>
+      <!--.l-header-region -->
+      <section class="l-button_menu-region">
+        <div class="row">
+          <div class="medium-9 small-12 columns right">
+            <?php print render($page['button_menu']); ?>
+          </div>
+        </div>
+      </section>
+      <!--/.l-button_menu-region -->
+    <?php endif; ?>
+
+  
+    <?php if (!empty($page['button_submenu'])): ?>
+    <!--/.l-button_submenu -->
+    <section class="l-button_submenu row">
+      <div class="medium-4 columns">
+        &nbsp;
+      </div>
+      <div class="medium-4 columns">
+        <?php print render($page['button_submenu']); ?>
+      </div>
+      <div class="medium-4 columns">
+        &nbsp;
+      </div>
+      
+    </section>
+    <!--/.l-featured -->
+  <?php endif; ?>
+
+  <?php if (!empty($page['featured'])): ?>
+    <!--/.featured -->
+    <section class="l-featured row">
+      <div class="medium-12 columns">
+        <?php print render($page['featured']); ?>
+      </div>
+    </section>
+    <!--/.l-featured -->
+  <?php endif; ?>
+
+  <?php if ($messages && !$zurb_foundation_messages_modal): ?>
+    <!--/.l-messages -->
+    <section class="l-messages row">
+      <div class="medium-12 columns">
+        <?php if ($messages): print $messages; endif; ?>
+      </div>
+    </section>
+    <!--/.l-messages -->
+  <?php endif; ?>
+
+  <?php if (!empty($page['help'])): ?>
+    <!--/.l-help -->
+    <section class="l-help row">
+      <div class="medium-12 columns">
+        <?php print render($page['help']); ?>
+      </div>
+    </section>
+    <!--/.l-help -->
+  <?php endif; ?>
+
+  <main role="main" class="row l-main">
+    <div class="<?php print $main_grid; ?> main columns">
+      <?php if (!empty($page['highlighted'])): ?>
+        <div class="highlight panel callout">
+          <?php print render($page['highlighted']); ?>
+        </div>
+      <?php endif; ?>
+
+      <a id="main-content"></a>
+
+      <?php if ($breadcrumb): print $breadcrumb; endif; ?>
+
+      <?php if ($title && !$is_front): ?>
+        <?php print render($title_prefix); ?>
+        <h1 id="page-title" class="title"><?php print $title; ?></h1>
+        <?php print render($title_suffix); ?>
+      <?php endif; ?>
+
+      <?php if (!empty($tabs)): ?>
+        <?php print render($tabs); ?>
+        <?php if (!empty($tabs2)): print render($tabs2); endif; ?>
+      <?php endif; ?>
+
+      <?php if ($action_links): ?>
+        <ul class="action-links">
+          <?php print render($action_links); ?>
+        </ul>
+      <?php endif; ?>
+
+      <?php print render($page['content']); ?>
+    </div>
+    <!--/.main region -->
+
+    <?php if (!empty($page['sidebar_first'])): ?>
+      <aside role="complementary" class="<?php print $sidebar_first_grid; ?> sidebar-first columns sidebar">
+        <?php print render($page['sidebar_first']); ?>
+      </aside>
+    <?php endif; ?>
+
+    <?php if (!empty($page['sidebar_second'])): ?>
+      <aside role="complementary" class="<?php print $sidebar_sec_grid; ?> sidebar-second columns sidebar">
+        <?php print render($page['sidebar_second']); ?>
+      </aside>
+    <?php endif; ?>
+  </main>
+  <!--/.main-->
+
+  <?php if (!empty($page['triptych_first']) || !empty($page['triptych_middle']) || !empty($page['triptych_last'])): ?>
+    <!--.triptych-->
+    <section class="l-triptych row">
+      <div class="triptych-first medium-4 columns">
+        <?php print render($page['triptych_first']); ?>
+      </div>
+      <div class="triptych-middle medium-4 columns">
+        <?php print render($page['triptych_middle']); ?>
+      </div>
+      <div class="triptych-last medium-4 columns">
+        <?php print render($page['triptych_last']); ?>
+      </div>
+    </section>
+    <!--/.triptych -->
+  <?php endif; ?>
+
+  <?php if (!empty($page['footer_firstcolumn']) || !empty($page['footer_secondcolumn']) || !empty($page['footer_thirdcolumn']) || !empty($page['footer_fourthcolumn'])): ?>
+    <!--.footer-sections -->
+    <section class="panel l-footer-sections">
+      <div class="row">
+        <?php if (!empty($page['footer_firstcolumn'])): ?>
+        <div class="footer-first medium-10 small-12 columns">
+          <?php print render($page['footer_firstcolumn']); ?>
+        </div>
+      </div>
+      <?php endif; ?>
+      <?php if (!empty($page['footer_secondcolumn'])): ?>
+        <div class="footer-second medium-3 small-12 columns">
+          <?php print render($page['footer_secondcolumn']); ?>
+        </div>
+      <?php endif; ?>
+    
+   
+    <!--/.footer-sections-->
+  <?php endif; ?>
+
+  <!--.l-footer-->
+  <footer class="l-footer row" role="contentinfo">
+    <?php if (!empty($page['footer'])): ?>
+      <div class="footer medium-12 columns">
+        <?php print render($page['footer']); ?>
+      </div>
+    <?php endif; ?>
+
+    <?php if ($site_name) :?>
+      <div class="copyright medium-12 columns">
+        &copy; 2008 - <?php print date('Y') . ' ' . check_plain($site_name) . ' ' . t('All rights reserved.'); ?>
+      </div>
+    <?php endif; ?>
+  </footer>
+   </section>
+  <!--/.footer-->
+
+
+  <?php if ($messages && $zurb_foundation_messages_modal): print $messages; endif; ?>
+</div></div>
+<!--/.page -->
+
+<script type="text/javascript">
+    /* <![CDATA[ */ 
+    TypekitConfig = {
+        kitId: 'kcl2oty'
+    };
+    (function() {
+        var tk = document.createElement('script');
+        tk.src = '//use.typekit.com/' + TypekitConfig.kitId + '.js';
+        tk.type = 'text/javascript';
+        tk.async = 'true';
+        tk.onload = tk.onreadystatechange = function() {
+            var rs = this.readyState;
+            if (rs && rs != 'complete' && rs != 'loaded') return;
+            try { Typekit.load(TypekitConfig); } catch (e) {}
+        };
+        var s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(tk, s);
+    })(); 
+    /* ]]> */
+</script>
+<!--/.typekit -->
