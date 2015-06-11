047361a5fd77e451b309e94fd8335629abe41469
Your Name <you@example.com>
2015-06-11 00:36:31 -0400

    Adding facebook tracking pixel

--- sites/all/themes/custom/cchi_v2/templates/page.tpl.php
+++ sites/all/themes/custom/cchi_v2/templates/page.tpl.php
@@ -268,3 +268,20 @@
     /* ]]> */
 </script>
 <!--/.typekit -->
+
+<script>(function() {
+var _fbq = window._fbq || (window._fbq = []);
+if (!_fbq.loaded) {
+var fbds = document.createElement('script');
+fbds.async = true;
+fbds.src = '//connect.facebook.net/en_US/fbds.js';
+var s = document.getElementsByTagName('script')[0];
+s.parentNode.insertBefore(fbds, s);
+_fbq.loaded = true;
+}
+_fbq.push(['addPixelId', '403874919798087']);
+})();
+window._fbq = window._fbq || [];
+window._fbq.push(['track', 'PixelInitialized', {}]);
+</script>
+<noscript><img height="1" width="1" alt="" style="display:none" src="https://www.facebook.com/tr?id=403874919798087&amp;ev=PixelInitialized" /></noscript>
